8c74ee0e1894070b8c512076dbdbd2c9b83467e2
SamaneM <saman9452@gmail.com>
2024-02-05 19:00:06 -0500

    add navbar

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './navbar.css';
+import logo from '../../assets/logo.png';
+import {Link} from 'react-scroll';
 
 const Navbar = () => {
   return (
-    <div>navbar</div>
+    <nav className='navbar' >
+      <img src={logo} className='logo'></img>
+      <div className='desktopMenu'>
+        <Link className='desktopMenuItem'>Home</Link>
+        <Link className='desktopMenuItem'>About Me</Link>
+        <Link className='desktopMenuItem'>Projects</Link>
+        <Link className='desktopMenuItem'>CV</Link>
+      </div>
+      <button className='desktopMenuBtn'>Contact Me</button>
+
+    </nav>
   )
 }
 
